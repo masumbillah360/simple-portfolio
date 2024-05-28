b56b82eac7fa1a176e5a96c0d389ba30a0f787f8
Masum Billah <masumbillah6584@gmail.com>
2024-05-28 20:16:07 +0600

    added all sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import './css/card.scss';
+import './css/globals.scss';
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Masum Billah",
-  description: "A fullstack web developer from Bangladesh",
+    title: 'Masum Billah - Web Developer',
+    description:
+        'A fullstack web developer from Bangladesh. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.',
 };
 
 export default function RootLayout({
@@ -15,8 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+          <body className={inter.className}>
+              <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+                  <Navbar />
+                  {children}
+              </main>
+              <Footer />
+          </body>
+      </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,53 @@
-import Image from "next/image";
+import AboutSection from '@/components/homepage/about';
+import Blog from '@/components/homepage/blog';
+import ContactSection from '@/components/homepage/contact';
+import Education from '@/components/homepage/education';
+import Experience from '@/components/homepage/experience';
+import HeroSection from '@/components/homepage/hero-section';
+import Projects from '@/components/homepage/projects';
+import Skills from '@/components/homepage/skills';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Masum Billah</h1>
-    </main>
-  );
+    const blogs = [
+        {
+            title: 'Title',
+        },
+        {
+            title: 'Title',
+        },
+        {
+            title: 'Title',
+        },
+        {
+            title: 'Title',
+        },
+        {
+            title: 'Title',
+        },
+        {
+            title: 'Title',
+        },
+        {
+            title: 'Title',
+        },
+        {
+            title: 'Title',
+        },
+        {
+            title: 'Title',
+        },
+    ];
+    return (
+        <>
+            <HeroSection />
+            <AboutSection />
+            <Experience />
+            <Skills />
+            <Projects />
+            <Education />
+            <Blog blogs={blogs} />
+            <ContactSection />
+        </>
+    );
 }
